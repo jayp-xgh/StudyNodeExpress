c8ea90a386bc1b3fcfc44f3012daa4c91f81b9e1
jayp-zip <joaovitorpereiramartins.2004@gmail.com>
2023-06-07 19:00:28 -0300

    [feat] config dbconnection

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,27 +1,28 @@
+const asyncHenler = require('express-async-handler');
 const getContacts = (req, res) => {
-  res.status(200).json({message: 'Get all contacts'});
+    res.status(200).json({message: 'Get all contacts'});
 };
 
-const createContact = async (req, res) => {
-  console.log(`the body is : ${req.body}`);
-  const {name, email, phone } = req.body;
-  if(!name || !email ||!phone){
-    res.status(400);
-    throw new Error("All fields are mendatory !");
-  }
-  res.status(201).json({message: 'Create Contact'});
-};
+const createContact =  asyncHenler(async (req, res) => {
+    console.log(`the body is : ${req.body}`);
+    const {name, email, phone } = req.body;
+    if(!name || !email ||!phone){
+        res.status(400);
+        throw new Error("All fields are mendatory !");
+    }
+    res.status(201).json({message: 'Create Contact'});
+});
 
-const getContact = async (req, res) => {
-  res.status(200).json({message: `Get constact for ${req.params.id}`});
-}
+const getContact =  asyncHenler(async (req, res) => {
+    res.status(200).json({message: `Get constact for ${req.params.id}`});
+});
 
-const updateContact = async (req, res) => {
-  res.status(200).json({message: `Upate contact for ${req.params.id}`});
-}
+const updateContact =  asyncHenler(async (req, res) => {
+    res.status(200).json({message: `Upate contact for ${req.params.id}`});
+});
 
-const deleteContact = async  (req, res) => {
-  res.status(200).json({message: `Delete contact for ${req.params.id}`});
-}
+const deleteContact =  asyncHenler(async  (req, res) => {
+    res.status(200).json({message: `Delete contact for ${req.params.id}`});
+});
 
 module.exports = {  getContacts, getContact, createContact, updateContact, deleteContact};
--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
+const connectDb = require('./config/dbConnection');
 const errorHendler = require('./middleware/errorHandler');
 const dotenv = require('dotenv').config();
+
+
+connectDb();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use('/api/contacts', require('./routes/contactRoutes'));
-app.unsubscribe(errorHendler);
-
+app.use(errorHendler);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
